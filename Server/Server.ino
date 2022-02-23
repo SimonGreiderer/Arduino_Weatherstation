722892abc6cd0b26262700a7a0182d7bd949a55d
Hattinger04 <s.8greiderer@gmail.com>
2022-02-23 10:33:13 +0100

    adding freeRtos (working)

--- Server/Server.ino
+++ Server/Server.ino
@@ -1,4 +1,8 @@
-
+#if CONFIG_FREERTOS_UNICORE
+#define ARDUINO_RUNNING_CORE 0
+#else
+#define ARDUINO_RUNNING_CORE 1
+#endif
 #include <esp_now.h>
 #include <WiFi.h>
 
@@ -26,13 +30,39 @@ void OnDataRecv(const uint8_t * mac_addr, const uint8_t *incomingData, int len) 
   // Update the structures with the new incoming data
   boardsStruct[myData.id-1].x = myData.x;
   boardsStruct[myData.id-1].y = myData.y;
-  Serial.printf("Temperatur value: %d \n", boardsStruct[myData.id-1].x);
-  Serial.printf("Humidity value: %d \n", boardsStruct[myData.id-1].y);
+  Serial.printf("Temperatur value: %g \n", boardsStruct[myData.id-1].x);
+  Serial.printf("Humidity value: %g \n", boardsStruct[myData.id-1].y);
   Serial.println();
 }
  
 void setup() {
   Serial.begin(115200);
+  setupLedPanel(); 
+  setupReceivingData(); 
+  xTaskCreatePinnedToCore(
+    receivingData
+    ,  "ReceivingData"   // A name just for humans
+    ,  1024  // This stack size can be checked & adjusted by reading the Stack Highwater
+    ,  NULL
+    ,  2  // Priority, with 3 (configMAX_PRIORITIES - 1) being the highest, and 0 being the lowest.
+    ,  NULL 
+    ,  ARDUINO_RUNNING_CORE);
+
+  xTaskCreatePinnedToCore(
+    ledPanel
+    ,  "LedPanel"
+    ,  1024  // Stack size
+    ,  NULL
+    ,  1  // Priority
+    ,  NULL 
+    ,  ARDUINO_RUNNING_CORE);
+}
+
+void setupLedPanel() {
+  // Do nothing right now
+}
+
+void setupReceivingData() {
   WiFi.mode(WIFI_STA);
   if (esp_now_init() != ESP_OK) {
     Serial.println("Error initializing ESP-NOW");
@@ -41,10 +71,25 @@ void setup() {
   esp_now_register_recv_cb(OnDataRecv);
 }
 
+void ledPanel(void *pvParameters) {
+   (void) pvParameters;
+   for (;;) {
+    // do nothing right now
+    Serial.println("LED-Panel");
+    vTaskDelay(2500);
+   }
+}
+
+void receivingData(void *pvParameters) {
+  (void) pvParameters;
+  for (;;) {
+    // Acess the variables for each board
+    float board1X = boardsStruct[0].x;
+    float board1Y = boardsStruct[0].y;
+    vTaskDelay(5000);
+  }
+}
 
 void loop() {
-  // Acess the variables for each board
-  float board1X = boardsStruct[0].x;
-  float board1Y = boardsStruct[0].y;
-  delay(5000);  
+  // Do nothing
 }
