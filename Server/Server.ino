2eaf103052cf4f83d87e059e6ea20bb465b75a8c
Hattinger04 <s.8greiderer@gmail.com>
2022-02-23 12:25:59 +0100

    finishing work for today

--- Server/Server.ino
+++ Server/Server.ino
@@ -5,6 +5,20 @@
 #endif
 #include <esp_now.h>
 #include <WiFi.h>
+#include <FastLED.h>
+#include <LEDMatrix.h>
+#include <LEDText.h>
+#include <FontRobotron.h>
+
+// Change the next 6 defines to match your matrix type and size
+
+#define LED_PIN        4
+#define COLOR_ORDER    GRB
+#define CHIPSET        WS2812B
+
+#define MATRIX_WIDTH   32
+#define MATRIX_HEIGHT  -8
+#define MATRIX_TYPE    VERTICAL_ZIGZAG_MATRIX // Wie sind die LEDs angeordnet
 
 
 // MAC: 4C:11:AE:CB:6D:74
@@ -12,12 +26,19 @@ typedef struct struct_message {
   int id;
   float x;
   float y;
-}struct_message;
+} struct_message;
 
+cLEDMatrix<MATRIX_WIDTH, MATRIX_HEIGHT, MATRIX_TYPE> leds;
+
+cLEDText ScrollingMsg;
 // Create a struct_message called myData
 struct_message myData;
 struct_message board1;
 struct_message boardsStruct[1] = {board1};
+
+const unsigned char TxtDemo[] = { EFFECT_SCROLL_LEFT EFFECT_HSV_CV "\x00\xff\xff\x40\xff\xff" "TEMPERATUR"
+};
+
 
 void OnDataRecv(const uint8_t * mac_addr, const uint8_t *incomingData, int len) {
   char macStr[18];
@@ -28,24 +49,24 @@ void OnDataRecv(const uint8_t * mac_addr, const uint8_t *incomingData, int len) 
   memcpy(&myData, incomingData, sizeof(myData));
   Serial.printf("Board ID %u: %u bytes\n", myData.id, len);
   // Update the structures with the new incoming data
-  boardsStruct[myData.id-1].x = myData.x;
-  boardsStruct[myData.id-1].y = myData.y;
-  Serial.printf("Temperatur value: %g \n", boardsStruct[myData.id-1].x);
-  Serial.printf("Humidity value: %g \n", boardsStruct[myData.id-1].y);
+  boardsStruct[myData.id - 1].x = myData.x;
+  boardsStruct[myData.id - 1].y = myData.y;
+  Serial.printf("Temperatur value: %g \n", boardsStruct[myData.id - 1].x);
+  Serial.printf("Humidity value: %g \n", boardsStruct[myData.id - 1].y);
   Serial.println();
 }
- 
+
 void setup() {
   Serial.begin(115200);
-  setupLedPanel(); 
-  setupReceivingData(); 
+  setupLedPanel();
+  setupReceivingData();
   xTaskCreatePinnedToCore(
     receivingData
     ,  "ReceivingData"   // A name just for humans
     ,  1024  // This stack size can be checked & adjusted by reading the Stack Highwater
     ,  NULL
     ,  2  // Priority, with 3 (configMAX_PRIORITIES - 1) being the highest, and 0 being the lowest.
-    ,  NULL 
+    ,  NULL
     ,  ARDUINO_RUNNING_CORE);
 
   xTaskCreatePinnedToCore(
@@ -54,12 +75,20 @@ void setup() {
     ,  1024  // Stack size
     ,  NULL
     ,  1  // Priority
-    ,  NULL 
+    ,  NULL
     ,  ARDUINO_RUNNING_CORE);
 }
 
 void setupLedPanel() {
-  // Do nothing right now
+  FastLED.addLeds<CHIPSET, LED_PIN, COLOR_ORDER>(leds[0], leds.Size());
+  FastLED.setBrightness(10); // WICHTIG - Hier wird die Helligkeit eingestellt. Am Anfang einen niedrigen Wert verwenden, und langsam hochtasten.
+  FastLED.clear(true);
+  delay(500);
+  ScrollingMsg.SetFont(RobotronFontData);
+  ScrollingMsg.Init(&leds, leds.Width(), ScrollingMsg.FontHeight() + 1, 0, 0);
+  ScrollingMsg.SetText((unsigned char *)TxtDemo, sizeof(TxtDemo) - 1);
+  ScrollingMsg.SetTextColrOptions(COLR_RGB | COLR_SINGLE, 0xff, 0x00, 0xff);
+
 }
 
 void setupReceivingData() {
@@ -69,15 +98,26 @@ void setupReceivingData() {
     return;
   }
   esp_now_register_recv_cb(OnDataRecv);
+  boardsStruct[0].x = 0; 
+  boardsStruct[0].y = 0; 
 }
 
 void ledPanel(void *pvParameters) {
-   (void) pvParameters;
-   for (;;) {
-    // do nothing right now
-    Serial.println("LED-Panel");
-    vTaskDelay(2500);
-   }
+  (void) pvParameters;
+  for (;;) {
+    if (ScrollingMsg.UpdateText() == -1) {
+      unsigned char temperatur[17];
+      sprintf((char *)temperatur, " WARM: %g", boardsStruct[0].x);
+      ScrollingMsg.SetText((unsigned char *)TxtDemo, sizeof(TxtDemo) - 1);
+      ScrollingMsg.SetScrollDirection(SCROLL_LEFT);
+      ScrollingMsg.SetTextColrOptions(COLR_HSV | COLR_GRAD_CH, 0x00, 0xff, 0xff, 0x40, 0xff, 0xff);
+      ScrollingMsg.SetText(temperatur, sizeof(temperatur) -1);
+      vTaskDelay(20);
+    }
+    else
+      FastLED.show();
+      vTaskDelay(20);
+  }
 }
 
 void receivingData(void *pvParameters) {
