0d46b4b85b3a33b3a4a9268c32c95ac5b2daab12
Hattinger04 <s.8greiderer@gmail.com>
2022-03-02 11:31:54 +0100

    finishing day

--- Server/Server.ino
+++ Server/Server.ino
@@ -9,8 +9,7 @@
 #include <LEDMatrix.h>
 #include <LEDText.h>
 #include <FontRobotron.h>
-
-// Change the next 6 defines to match your matrix type and size
+#include <ThingsBoard.h>
 
 #define LED_PIN        4
 #define COLOR_ORDER    GRB
@@ -20,6 +19,16 @@
 #define MATRIX_HEIGHT  -8
 #define MATRIX_TYPE    VERTICAL_ZIGZAG_MATRIX // Wie sind die LEDs angeordnet
 
+#define COUNT_OF(x) ((sizeof(x)/sizeof(0[x])) / ((size_t)(!(sizeof(x) % sizeof(0[x])))))
+#define WIFI_AP_NAME        "SSID"
+#define WIFI_PASSWORD       "PW"
+#define TOKEN               "Weatherstation"
+#define THINGSBOARD_SERVER  "demo.thingsboard.io"
+
+WiFiClient espClient;
+ThingsBoard tb(espClient);
+int status = WL_IDLE_STATUS;
+bool subscribed = false;
 
 // MAC: 4C:11:AE:CB:6D:74
 typedef struct struct_message {
@@ -36,9 +45,7 @@ struct_message myData;
 struct_message board1;
 struct_message boardsStruct[1] = {board1};
 
-const unsigned char TxtDemo[] = { EFFECT_SCROLL_LEFT EFFECT_HSV_CV "\x00\xff\xff\x40\xff\xff" "TEMPERATUR"
-};
-
+const unsigned char TxtDemo[] = { EFFECT_SCROLL_LEFT EFFECT_HSV_CV "\x00\xff\xff\x40\xff\xff" "TEMPERATUR"};
 
 void OnDataRecv(const uint8_t * mac_addr, const uint8_t *incomingData, int len) {
   char macStr[18];
@@ -88,7 +95,6 @@ void setupLedPanel() {
   ScrollingMsg.Init(&leds, leds.Width(), ScrollingMsg.FontHeight() + 1, 0, 0);
   ScrollingMsg.SetText((unsigned char *)TxtDemo, sizeof(TxtDemo) - 1);
   ScrollingMsg.SetTextColrOptions(COLR_RGB | COLR_SINGLE, 0xff, 0x00, 0xff);
-
 }
 
 void setupReceivingData() {
@@ -98,8 +104,33 @@ void setupReceivingData() {
     return;
   }
   esp_now_register_recv_cb(OnDataRecv);
-  boardsStruct[0].x = 0; 
-  boardsStruct[0].y = 0; 
+  boardsStruct[0].x = 0;
+  boardsStruct[0].y = 0;
+}
+
+int calculatePixelNr(int x, int y) {
+  return x % 2 == 1 ? 8 * x + 7 - y : 8 * x + y;
+}
+
+void InitWiFi() {
+    Serial.println("Connecting to AP ...");
+    WiFi.begin(WIFI_AP_NAME, WIFI_PASSWORD);
+    while (WiFi.status() != WL_CONNECTED) {
+      vTaskDelay(500);
+      Serial.print(".");
+    }
+    Serial.println("Connected to AP");
+}
+
+void reconnect() {
+  status = WiFi.status();
+  if ( status != WL_CONNECTED) {
+    WiFi.begin(WIFI_AP_NAME, WIFI_PASSWORD);
+    while (WiFi.status() != WL_CONNECTED) {
+      vTaskDelay(500);
+    }
+    Serial.println("Connected to AP");
+  }
 }
 
 void ledPanel(void *pvParameters) {
@@ -111,23 +142,43 @@ void ledPanel(void *pvParameters) {
       ScrollingMsg.SetText((unsigned char *)TxtDemo, sizeof(TxtDemo) - 1);
       ScrollingMsg.SetScrollDirection(SCROLL_LEFT);
       ScrollingMsg.SetTextColrOptions(COLR_HSV | COLR_GRAD_CH, 0x00, 0xff, 0xff, 0x40, 0xff, 0xff);
-      ScrollingMsg.SetText(temperatur, sizeof(temperatur) -1);
+      ScrollingMsg.SetText(temperatur, sizeof(temperatur) - 1);
       vTaskDelay(20);
     }
     else
       FastLED.show();
-      vTaskDelay(20);
+    vTaskDelay(20);
   }
 }
 
 void receivingData(void *pvParameters) {
   (void) pvParameters;
   for (;;) {
+    setupReceivingData();
     // Acess the variables for each board
     float board1X = boardsStruct[0].x;
     float board1Y = boardsStruct[0].y;
+    sendingDataThingsboard(board1X, board1Y);
     vTaskDelay(5000);
   }
+}
+
+void sendingDataThingsboard(float temp, float hum) {
+  WiFi.begin(WIFI_AP_NAME, WIFI_PASSWORD);
+  InitWiFi();
+  if (WiFi.status() != WL_CONNECTED) {
+    reconnect();
+    return;
+  }
+  if (!tb.connected()) {
+    if (!tb.connect(THINGSBOARD_SERVER, TOKEN)) {
+      Serial.println("Failed to connect");
+      return;
+    }
+  }
+  Serial.println("Sending data...");
+  tb.sendTelemetryFloat("temperature", temp);
+  tb.sendTelemetryFloat("humidity", hum);
 }
 
 void loop() {
